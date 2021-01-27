c52a88a5a9c8eb947b64155f8e02ad297867489a
BCDeshiG <bcdg55@gmail.com>
2021-01-27 15:01:03 +0000

    Rotation and Collision
    Player now points at mouse and collides with level walls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
+	public Rigidbody2D rb;
 	public float moveSpeed = 4f;
 	private Vector2 movement;
+	private Vector2 mousePos; // Normalised distance from centre
+	private float angle = 0; // Player rotation 
 
 	// Update is called once per frame
 	void Update()
 	{
 		movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+		movement.y = Input.GetAxisRaw("Vertical");
+		mousePos.x = (Input.mousePosition.x / Screen.width) - 0.5f;
+		mousePos.y = (Input.mousePosition.y / Screen.height) - 0.5f;
+		angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
 	}
 
 	// Moves the player
 	void FixedUpdate()
 	{
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+		rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 	}
 }
