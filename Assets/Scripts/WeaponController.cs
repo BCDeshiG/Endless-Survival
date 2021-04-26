403d41e32fd994bf67c3fb29dfccc436727367f7
BCDeshiG <bcdg55@gmail.com>
2021-04-26 15:42:58 +0100

    Limited Ammo
    Also fix fast bullets collision bug.

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -7,14 +7,17 @@ public class WeaponController : MonoBehaviour
 	public GameObject bullet; // Type of ammunition used
 	private Rigidbody2D rb; // Collision of bullet
 	private bool firing = false;
-	public int attackDamage;
-	public int bulletSpeed;
-	public float fireRate;
+	public int attackDamage; // How much damage it inflicts
+	public int bulletSpeed; // How fast the bullets travel
+	public float fireRate; // Seconds per bullet
+	public int ammoCount; // How many bullets the player has
+	private HUD hud;
 
 	void Start()
 	{
 		rb = bullet.GetComponent<Rigidbody2D>();
 		bullet.GetComponent<BulletController>().attackDamage = attackDamage;
+		hud = GameObject.Find("HUD").GetComponent<HUD>();
 	}
 
 	// Coroutine for firing weapon
@@ -22,12 +25,25 @@ public class WeaponController : MonoBehaviour
 	{
 		if(!firing && !PauseController.isPaused)
 		{
-			firing = true;
-			Rigidbody2D clone = Instantiate(rb, pos, rot);
-			clone.gameObject.SetActive(true);
-			clone.velocity = clone.transform.right * bulletSpeed;
-			yield return new WaitForSeconds(fireRate);
-			firing = false;
+			if(ammoCount > 0)
+			{
+				firing = true;
+				// Decrement ammo count
+				ammoCount--;
+				// Create bullet
+				Rigidbody2D clone = Instantiate(rb, pos, rot);
+				clone.gameObject.SetActive(true);
+				// Fire bullet
+				clone.velocity = clone.transform.right * bulletSpeed;
+				// Cooldown
+				yield return new WaitForSeconds(fireRate);
+				firing = false;
+			}
+			else
+			{
+				hud.prompt("Out of " + bullet.name + " ammo!");
+				yield return new WaitForSeconds(fireRate);
+			}
 		}
 	}
 }
